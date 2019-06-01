424fb382244e702f9c5f403fdda64a05ea5678c3
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-05-31 18:13:11 -0600

    Add localStorage to JS

--- todo-list.js
+++ todo-list.js
@@ -1,5 +1,27 @@
-class todo {
-	constructor(obj) {
-      
+class toDoList {
+	constructor(id, title, tasks, urgent) {
+      this.id = id;
+      this.title = title;
+      this.tasks = tasks;
+      this.urgent = urgent || false;
+	}
+
+	saveToStorage(taskArray) {
+    	var stringifiedTasks = JSON.stringify(taskArray);
+    	localStorage.setItem('tasks', stringifiedTasks);
+	}
+
+	deleteFromStorage() {
+
+	}
+
+	updateToDo() {
+		// Update todos title and urgency
+
+	}
+
+	updateTask() {
+       // Update tasks content and if it has been completed
+
 	}
 }
